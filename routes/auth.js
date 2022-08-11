d04a47fb8db89798bfb0ac547ef3a6020f4732b4
kmal808 <kjmalley7@gmail.com>
2022-08-10 20:29:48 -1000

    added logout user and logout redirect

--- routes/auth.js
+++ routes/auth.js
@@ -17,4 +17,16 @@ router.get(
 	}
 )
 
+//* @desc Logout User
+//* @route /auth/logout
+
+router.get('/logout', (req, res, next) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err)
+		}
+		res.redirect('/')
+	})
+})
+
 module.exports = router
