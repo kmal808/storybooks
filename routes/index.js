76c63a27dd9a61b511919d0637843ab6c942ceb2
kmal808 <kjmalley7@gmail.com>
2022-08-11 22:09:57 -1000

    added error pages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const Story = require('../models/Story')
 
 //* @desc Login/Landing page
 //* @route GET /
@@ -13,10 +14,16 @@ router.get('/', ensureGuest, (req, res) => {
 
 //* @desc Dashboard
 //* @route GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-	res.render('Dashboard', {
-		name: req.user.displayName,
-	})
+router.get('/dashboard', ensureAuth, async (req, res) => {
+	try {
+		const stories = await Story.find({ user: req.user.id }).lean()
+		res.render('Dashboard', {
+			name: req.user.displayName,
+			stories,
+		})
+	} catch (error) {
+		console.error(error)
+	}
 })
 
 module.exports = router
