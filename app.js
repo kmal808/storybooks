83189730f6f89b0eaafc07b0e7bcbd97f5bba65d
kmal808 <kjmalley7@gmail.com>
2022-08-13 22:37:50 -1000

    added edit story button

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 //* Handlebars Helpers
-const { formatDate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
 
 //* Handlebars
 app.engine(
@@ -36,6 +36,9 @@ app.engine(
 	exphbs.engine({
 		helpers: {
 			formatDate,
+			stripTags,
+			truncate,
+			editIcon,
 		},
 		defaultLayout: 'main',
 		extname: '.hbs',
@@ -58,6 +61,12 @@ app.use(
 //* Passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//* Set global variable
+app.use(function (req, res, next) {
+	res.locals.user = req.user || null
+	next()
+})
 
 //* Static folder
 app.use(express.static(path.join(__dirname, 'public')))
