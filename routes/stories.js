2137e2dced4929d19467e9ab06bfd895e92e6eac
kmal808 <kjmalley7@gmail.com>
2022-08-15 23:13:42 -1000

    added delete button removes story

--- routes/stories.js
+++ routes/stories.js
@@ -47,42 +47,64 @@ router.get('/', ensureAuth, async (req, res) => {
 //* @route GET /stories/edit/:id
 
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-	const story = await Story.findOne({
-		_id: req.params.id,
-	}).lean()
+	try {
+		const story = await Story.findOne({
+			_id: req.params.id,
+		}).lean()
 
-	if (!story) {
-		return res.render('error/404')
-	}
+		if (!story) {
+			return res.render('error/404')
+		}
 
-	if (story.user != req.user.id) {
-		res.redirect('/stories')
-	} else {
-		res.render('stories/edit', {
-			story,
-		})
+		if (story.user != req.user.id) {
+			res.redirect('/stories')
+		} else {
+			res.render('stories/edit', {
+				story,
+			})
+		}
+	} catch (error) {
+		console.error('error/500')
 	}
 })
 
 //* @desc Update story
-//* @route GET /stories/:id
+//* @route PUT /stories/:id
 
 router.put('/:id', ensureAuth, async (req, res) => {
-	let story = await Story.findById(req.params.id).lean()
+	try {
+		let story = await Story.findById(req.params.id).lean()
 
-	if (!story) {
-		return res.render('error/404')
+		if (!story) {
+			return res.render('error/404')
+		}
+
+		if (story.user != req.user.id) {
+			res.redirect('/stories')
+		} else {
+			story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+				new: true,
+				runValidators: true,
+			})
+
+			res.redirect('/dashboard')
+		}
+	} catch (error) {
+		console.error(error)
+		return res.render('error/500')
 	}
+})
 
-	if (story.user != req.user.id) {
-		res.redirect('/stories')
-	} else {
-		story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-			new: true,
-			runValidators: true,
-		})
+//* @desc Delete story
+//* @route DELETE /stories/:id
 
+router.delete('/:id', ensureAuth, async (req, res) => {
+	try {
+		await Story.remove({ _id: req.params.id })
 		res.redirect('/dashboard')
+	} catch (error) {
+		console.error(error)
+		return res.render('error/500')
 	}
 })
 
